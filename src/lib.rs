826e4d35183c14f9dd81ca3349876003e5f3e3fe
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-04-11 11:18:28 -0700

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+// Import necessary modules
+pub mod primecheck;
+
+// Declare the crate as a workspace
+// This is required to enable the use of features across multiple crates in the workspace
+// For example, if the caesar feature is enabled in SadieFish, it will also be enabled in any other
+// crate in the workspace that depends on SadieFish.
+// This is useful for testing and for avoiding version mismatches between crates.
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(feature = "workspace")]
+pub mod workspace {
+    pub use ImtiazGermain_primecheck as primecheck;
+}
+
+// Declare the encryption module as the default feature
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(feature = "default")]
+pub use ImtiazGermain_primecheck::*;
+
+// Declare features for the various encryption algorithms and utilities
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(any(feature = "isprime", feature = "isgermainprime", feature="isimtiazgermainprime"))]
+#[cfg(not(feature = "default"))]
+pub use ImtiazGermain_primecheck::*;
